6fa6ed7e45034fcd1eceeaf4d5d9fc6caa8cf2e1
okram ovic <okram@protonmail.ch>
2017-11-22 16:13:35 +0100

    adding env etc

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", function(req,res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 
-    console.log('breathe1 listens on port 3000')
+    console.log('breathe1 listens on port 3000 or ' + process.env.PORT)
 })
