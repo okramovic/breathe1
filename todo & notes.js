bb2556d6aadae28f7c6988102899cca7718e2087
okram ovic <okram@protonmail.ch>
2017-11-26 12:56:16 +0100

    adding manifest.json + icons

---
+++ todo & notes.js
@@ -0,0 +1,37 @@
+/* 
+
+
+    manifest basic instructions
+
+        https://classroom.udacity.com/courses/ud811/lessons/8312251083/concepts/83106419190923
+
+        https://manifest-validator.appspot.com/
+
+
+
+
+    install banners
+
+        https://classroom.udacity.com/courses/ud811/lessons/8312251083/concepts/83106419240923
+
+
+
+
+    WPAs on safari
+
+        https://classroom.udacity.com/courses/ud811/lessons/8312251083/concepts/83106419250923
+
+        https://developer.apple.com/library/content/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
+
+
+
+    options to serve it
+      - github pages? what is it  \
+                                  |- both support static hosting over https
+      - firebase                  /
+
+      - google app engine   (on google cloud) - for back-end logic
+
+
+    create https server = have http server and add https certificate to it        
+*/
